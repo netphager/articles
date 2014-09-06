b2bf32a5b111c736bc3f2993aae6e46b1b37961a
netphager <netphager@gmail.com>
2014-09-06 09:21:28 +0300

    config

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = new (function(){
 
     var that = this;
     /* PATHS */
-    this.rootDir = '/store/Dropbox/www/node';
-    this.appDir = '/store/Dropbox/www/node/app/';
+    this.rootDir = '/store/Dropbox/www/articles';
+    this.appDir = '/store/Dropbox/www/articles/app/';
 /*    this.rootDir = 'D:\\work\\node';
     this.appDir = 'D:\\work\\node\\app\\';*/
     this.databaseDir = path.join(that.appDir,'database/');
