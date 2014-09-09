64a7960a0318513947aa83253edeaac88b710069
netphager <netphager@gmail.com>
2014-09-09 09:52:38 +0300

    adding some comments in init.js

--- init.js
+++ init.js
@@ -1,3 +1,4 @@
+/* =============================== LOADING LIBRARIES =============================== */
 var path = require('path');
 var express = require('express')
 var app = express();
@@ -6,32 +7,33 @@ var bodyParser = require('body-parser');
 var expressSession = require('express-session');
 var eventEmitter = new events.EventEmitter();
 var http = require('http').Server(app);
+var port = 3000;
 
 global.config = require('./config');
 global.debug = require(config.libDir+'debugger');
+/*loader module for loading controller,templates and layouts*/
 var loader = require(config.libDir+'loader');
+// db controller to access db
 var db = require(config.libDir+'database');
-
-var login = false;
+/*request validator*/
 requestValidator = require(config.libDir+'requestValidator');
 requestValidator.eventEmitter = eventEmitter;
 requestValidator.listen();
-
-
+/*init sessoin and bodyparser*/
 app.use('/public', express.static(config.appDir + '/public'));
 app.use(bodyParser.json())
 app.use(expressSession({secret: '123456qwerty'}));
-
+/* =============================== INITIAL APP =============================== */
+/*enable debug messages*/
 debug.init();
 
-// connect to a database
+/*connect to a database*/
 db.getInstance().connect('localhost');
 
+/*loading layout*/
 app.get('/'+config.mainApp,function(req,res) {
-    // req.session.isLogged = false;
     var outputHtml = '';
     var resStatus = 200;
-    // load layout
     var layout = loader.loadLayout();
 
     if(debug.getErrors().length == 0) {
@@ -44,56 +46,42 @@ app.get('/'+config.mainApp,function(req,res) {
     debug.clearErrors();
 });
 
+/*ajax calls for controllers*/
 var loadedControllers = {};
 
 for(var i in config.controllers) {
     app.all('/'+config.controllers[i]+'/:method', function(req,res) {
-        
+
         var controllerName = req.url.split('/')[1];
         var method = req.param('method');
+        if(!(controllerName in loadedControllers)) {
+            loadedControllers[controllerName] = loader.loadController(controllerName);
+            console.log('loading controller ' + controllerName);
+        }
 
-
-        // eventEmitter.on('requestValidator',function(requestData) {
-            // if(requestData.isValidRequest === true) {
-                if(!(controllerName in loadedControllers)) {
-                    loadedControllers[controllerName] = loader.loadController(controllerName);
-                    console.log('loading controller ' + controllerName);
-                }
-
-                loadedControllers[controllerName][method](req,res);        
-            // }
-        // });
-        
-        // eventEmitter.emit('request',{req:req,res:res,requestedPage:method});
-
+        loadedControllers[controllerName][method](req,res);
     });
 }
 
+/*router request*/
 app.get('/router.js', function(req,res) {
     var fs = require('fs');
     res.send(fs.readFileSync(config.libDir+'router.js', 'utf-8'));
-    // eventEmitter.emit('request',{req:req,res:res});
 });
 
+/*loading templates request*/
 app.post('/loadTemplate',function(req,res) {
-
     var controllerName = req.body.controllerName;
     var templateName = req.body.templateName;
     var method = req.body.method;
-
-    // eventEmitter.on('requestValidator',function(requestData) {
-        // if(requestData.isValidRequest === true) {
-            var outputHtml = loader.loadTemplate(templateName,controllerName);
-            if(outputHtml === false) {
-                outputHtml = debug.getErrorsStr();
-            }
-            res.send(outputHtml);
-        // }
-    // });
-
-    // eventEmitter.emit('request',{req:req,res:res,requestedPage: method});
+    var outputHtml = loader.loadTemplate(templateName,controllerName);
+    if(outputHtml === false) {
+        outputHtml = debug.getErrorsStr();
+    }
+    res.send(outputHtml);
 });
 
-http.listen(3000, function(){
-  debug.log('listening on *:3000');
+/*start server*/
+http.listen(port, function(){
+  debug.log('listening on *:'+port);
 });
