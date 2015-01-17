2ea9e398b0cb5ad51ba937df7c9684605d7992d0
netphager <netphager@gmail.com>
2015-01-17 11:00:56 +0200

    config

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ module.exports = new (function(){
 
     var that = this;
     /* PATHS */
-    /*this.rootDir = '/store/Dropbox/www/articles';
-    this.appDir = '/store/Dropbox/www/articles/app/';*/
-    this.rootDir = 'D:\\work\\articles\\';
-    this.appDir = 'D:\\work\\articles\\app\\';
+    this.rootDir = '/store/Dropbox/www/articles';
+    this.appDir = '/store/Dropbox/www/articles/app/';
+    /*this.rootDir = 'D:\\work\\articles\\';
+    this.appDir = 'D:\\work\\articles\\app\\';*/
     this.databaseDir = path.join(that.appDir,'database/');
-    // this.libDir = path.join('/store/Dropbox/www/framework/');
-    this.libDir = path.join('D:\\work\\framework\\');
+    this.libDir = path.join('/store/Dropbox/www/framework/');
+    // this.libDir = path.join('D:\\work\\framework\\');
     this.templatesDir = 'http://localhost:3000/templates/{controller}/';
 
     /* PROPERTIES */
